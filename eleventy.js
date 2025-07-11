a2f129dec473c65c65b0a683b111ff9c8d81b430
Richard Lawrence <richlaw1986@gmail.com>
2025-07-11 21:54:54 +0100

    try again

--- eleventy.js
+++ eleventy.js
@@ -1,11 +1,20 @@
 module.exports = function(eleventyConfig) {
+  // Copy public assets
   eleventyConfig.addPassthroughCopy("public");
+  
+  // Add Nunjucks as template engine
+  eleventyConfig.addTemplateFormats("njk");
   
   return {
     dir: {
       input: "src",
       output: "_site",
-      includes: "_includes"
-    }
+      includes: "_includes",
+      layouts: "_includes",
+      data: "_data"
+    },
+    templateFormats: ["md", "njk", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk"
   };
 };
