82525d57b0d1ce432cd1dda46173ce0b28166676
Richard Lawrence <richlaw1986@gmail.com>
2025-07-11 22:01:58 +0100

    changing elenvty file again

--- eleventy.js
+++ eleventy.js
@@ -1,12 +1,20 @@
+const path = require("path");
+
 module.exports = function(eleventyConfig) {
-  // Copy public assets
   eleventyConfig.addPassthroughCopy("public");
+  
+  // Set the includes directory explicitly
+  eleventyConfig.setLayoutsDirectory("src/_includes");
   
   return {
     dir: {
       input: "src",
       output: "_site",
       includes: "_includes"
-    }
+    },
+    templateFormats: ["md", "njk", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk"
   };
 };
