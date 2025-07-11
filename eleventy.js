647934894b9d45b0d7e4cff4bbdb228b9a494d72
Richard Lawrence <richlaw1986@gmail.com>
2025-07-11 21:58:20 +0100

    simplifying the elenty file

--- eleventy.js
+++ eleventy.js
@@ -2,19 +2,11 @@ module.exports = function(eleventyConfig) {
   // Copy public assets
   eleventyConfig.addPassthroughCopy("public");
   
-  // Add Nunjucks as template engine
-  eleventyConfig.addTemplateFormats("njk");
-  
   return {
     dir: {
       input: "src",
       output: "_site",
-      includes: "_includes",
-      layouts: "_includes",
-      data: "_data"
-    },
-    templateFormats: ["md", "njk", "html"],
-    markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk"
+      includes: "_includes"
+    }
   };
 };
